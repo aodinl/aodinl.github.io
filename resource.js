8c10b53925084f7d9148bfaeb268a0ca1dfbf2e0
aodinl <74182232+aodinl@users.noreply.github.com>
2025-05-26 12:08:00 +0800

    Update resource.js

--- resource.js
+++ resource.js
@@ -34,5 +34,6 @@ let bookmarkList = [
 
 let irokunnrennList = [
                        {meisyou: "猜颜色", website:'./iroatari.html'}, 
-                       {meisyou: '明度饱和度', website:'./saturationandvalue.html'}
+                       {meisyou: '明度饱和度', website:'./saturationandvalue.html'},
+                       {meisyou: '顔色明度飽和度', website:'./irosaturationandvalue.html'}
                        ]
